68ed1cb9e3ad4713390543508ea5e1a63d722f10
Kal <134562542+TheRealObamacare@users.noreply.github.com>
2024-09-11 07:58:51 -0500

    Accessed on is done

--- src/AccessedOn.java
+++ src/AccessedOn.java
@@ -1,16 +1,21 @@
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.io.*;
-public class AccessedOn
-{
-    public class Main {
-        public static void main(String[] args) {
-            FileWriter =
+
+public class AccessedOn {
+        public static void main(String[] args) throws IOException {
+            File yay = new File("AccessedOn.txt");
+            if (!yay.exists())
+                yay.createNewFile();
+            FileWriter bruh = new FileWriter(yay, true);
+            PrintWriter yes = new PrintWriter(bruh);
             LocalDateTime myDateObj = LocalDateTime.now();
-            System.out.println("Before Formatting: " + myDateObj);
-            DateTimeFormatter myFormatObj = DateTimeFormatter.ofPattern("E, MMM dd yyyy HH:mm:ss");
+            DateTimeFormatter myFormatObj = DateTimeFormatter.ofPattern("E, MMM dd HH:mm:ss yyyy");
             String formattedDate = myDateObj.format(myFormatObj);
-            System.out.println("After Formatting: " + formattedDate);
+            FileReader huh = new FileReader(yay);
+            BufferedReader br = new BufferedReader(huh);
+            yes.println(formattedDate.substring(0, formattedDate.length()-4) + "CDT " + formattedDate.substring(formattedDate.length()-4));
+            bruh.close();
+            yes.close();
         }
-    }
 }
